879c15ce0db09c3402e86ddfcc39934aba7d012b
Mohit <132772017+Mohit8817@users.noreply.github.com>
2026-02-21 10:36:49 +0530

    added menu

--- frontend/src/jsx/layouts/nav/Menu.jsx
+++ frontend/src/jsx/layouts/nav/Menu.jsx
@@ -1,66 +1,11 @@
 export const MenuList = [
-    //Dashboard
-    {
-        title: 'Dashboard',
-        classsChange: 'mm-collapse',
-        iconStyle: <i className="flaticon-025-dashboard"></i>,
-        content: [
-            {
-                title: 'Dashboard Light',
-                to: 'dashboard',
-            },
-            {
-                title: 'Dashboard Dark',
-                to: 'dashboard-dark',
-            },
-            // {
-            //     title: 'Dashboard 3',
-            //     to: 'index-3',
-            // },
-            // {
-            //     title: 'Dashboard 4',
-            //     to: 'index-4',
-            // },
-            // {
-            //     title: 'Dashboard 5',
-            //     to: 'index-5',
-            // },
-            // {
-            //     title: 'Dashboard 6',
-            //     to: 'index-6',
-            // },
-            // {
-            //     title: 'Dashboard 7',
-            //     to: 'index-7',
-            // },
-            // {
-            //     title: 'Dashboard 8',
-            //     to: 'index-8',
-            // },
-            // {
-            //     title: 'My Wallet',
-            //     to: 'my-wallet',
-            // },
-            // {
-            //     title: 'Invoices',
-            //     to: 'invoices',
-            // },
-
-            // {
-            //     title: 'Cards Center',
-            //     to: 'cards-center',
-            // },
-            // {
-            //     title: 'Transaction',
-            //     to: 'transaction-history',
-            // },
-
-            // {
-            //     title: 'Transaction Details',
-            //     to: 'transaction-details',
-            // },
-        ],
-    },
+// Dashboard
+{
+    title: 'Dashboard',
+    classsChange: '',
+    iconStyle: <i className="flaticon-025-dashboard"></i>,
+    to: 'dashboard',   
+},
 
 
 // Panel Generation
